55a3e00a89e85f6f955deebe43f766d72032f811
Mahsa <m.hamedd1380@gmail.com>
2023-07-03 20:24:42 +0330

    Initial commit

---
+++ minMax/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace maxMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter a number: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                numbers.Add(number);
+            }
+            {
+                int result = numbers.Max();
+                Console.WriteLine("The max number is " + numbers.Max());
+
+                int answer = numbers.Min();
+                Console.WriteLine("The min number is " + numbers.Min());
+            }
+
+        }
+    }
+}
